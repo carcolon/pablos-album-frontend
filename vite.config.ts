21433ae38695e552efa110ba4c27b77db8cd2a34
Carlos Colón <cfca5@hotmail.com>
2026-09-19 17:37:51 -0500

    Point local proxy to API launch port

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5152',
+        target: 'http://localhost:5188',
         changeOrigin: true,
         secure: false,
       },
